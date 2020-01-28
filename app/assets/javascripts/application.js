d539c0b17ff0280a8ef69f1051c1fbe88b80f7c5
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-01-28 13:35:31 +0900

    Fix: ナビバー/サイドバーのviewを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,10 +36,9 @@ $(document).ready(function(){
 });
 
 
+// Sidenav
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('#sidenav-1').sidenav({ edge: 'left' });
     $('#sidenav-2').sidenav({ edge: 'right' });
 });
-
-
