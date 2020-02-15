45b970b227aa7fbb4993814a874e8253bb735cfc
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-02-15 17:36:47 +0900

    Add: ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
   resources :users, only: [:index, :show] do
     get :relations_index
