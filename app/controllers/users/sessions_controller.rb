c67224bfdacf4862ac38eea3cb3452d0cb796eea
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-02-16 12:54:07 +0900

    Fix: 細かい修正(アイコン変更/文言の変更etc)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーのため、アカウントの変更・削除はできません。'
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
    #GET /resource/sign_in
    #def new
