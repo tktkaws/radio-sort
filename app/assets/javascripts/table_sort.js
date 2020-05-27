21670df1c577de530522f7df240d8c41b5b99f40
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-05-27 18:54:50 +0900

    Fix: ユーザー詳細 / ラジオ詳細のviewを修正

--- app/assets/javascripts/table_sort.js
+++ app/assets/javascripts/table_sort.js
@@ -13,8 +13,7 @@ $(function(){
             });
         },
         stop: function(e, ui){
-            ui.item.children('td').not('.item__status').effect('highlight', { color: "#0097a7" }, 500)
-            ui.item.children('th').not('.item__status').effect('highlight', { color: "#0097a7" }, 500)
+            $('#likes_list').find('[name="num_data"]').effect('highlight', { color: "#b2dfdb" }, 500)
             $('#likes_list').find('[name="num_data"]').each(function(idx){
                 $(this).html(`${idx+1}位`);
             });
