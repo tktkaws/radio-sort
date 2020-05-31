b3d480f226974cad19acba590bbd4b41d6af3889
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-05-31 15:52:40 +0900

    Fix： viewの細かい修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,11 @@
 class TopController < ApplicationController
   def index
     @q = Radio.ransack(params[:q])
-    @radios = Radio.all
+    @result = @q.result
+    @points = @result.joins(:likes).limit(10).order('sum_point desc').group(:radio_id).sum(:point).values
+    @radios = Radio.find(@result.joins(:likes).limit(10).order('sum_point desc').group(:radio_id).sum(:point).keys)
+    @radios = Kaminari.paginate_array(@radios).page(params[:page])
+
+    @users = User.order("RANDOM()").limit(4)
   end
 end
