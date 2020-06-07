18fe0c5ea2cf5dbdeb4e20813ca9786ab85c963e
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-06-07 13:29:56 +0900

    Fix: comment_spec

--- spec/system/comment_spec.rb
+++ spec/system/comment_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
-#RSpec.describe Comment, type: :system do
-#  before(:all) do
-#    FactoryBot.reload
-#    @user = create(:user)
-#    @radio = create(:radio)
-#    @comment = create(:comment)
-#  end
-#  after(:all) do
-#    DatabaseCleaner.clean_with(:truncation)
-#    FactoryBot.reload
-#  end
-#
-#  describe 'コメント機能' do
-#    before do
-#      visit new_user_session_path
-#      fill_in 'user[email]', with: '1@test.com'
-#      fill_in 'user[password]', with: '111111'
-#      click_on 'login'
-#    end
-#    it 'コメントが投稿されること' do
-#      visit radio_path(@user)
-#      fill_in 'textarea1', with: 'テストコメント'
-#      click_on '投稿'
-#      sleep 1
-#      expect(page).to have_content 'テストコメント'
-#    end
-#
-#    it 'コメントが削除されること' do
-#      visit radio_path(@user)
-#      find('#comments_area a[data-method=delete]').click
-#      sleep 1
-#      expect(page).to_not have_content '生涯ベスト'
-#    end
-#  end
-#end
+RSpec.describe Comment, type: :system do
+  before(:all) do
+    FactoryBot.reload
+    @user = create(:user)
+    @radio = create(:radio)
+    @comment = create(:comment)
+  end
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation)
+    FactoryBot.reload
+  end
+
+  describe 'コメント機能' do
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: '1@test.com'
+      fill_in 'user[password]', with: '111111'
+      click_on 'login'
+    end
+    it'コメントが投稿されること' do
+      visit radio_path(@user)
+      find("textarea.materialize-textarea").set("テストコメント")
+      click_on '投稿'
+      sleep 1
+      expect(page).to have_content 'テストコメント'
+    end
+
+    it 'コメントが削除されること' do
+      visit radio_path(@user)
+      find('#comments_area a[data-method=delete]').click
+      sleep 1
+      expect(page).to_not have_content '生涯ベスト'
+    end
+  end
+end
